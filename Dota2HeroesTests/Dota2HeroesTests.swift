dbdcb5af8027973c0cf1cb45d01d478236eb81f6
Denis <the.22104929@gmail.com>
2021-08-12 09:26:16 +0700

    first

---
+++ Dota2HeroesTests/Dota2HeroesTests.swift
@@ -0,0 +1,33 @@
+//
+//  Dota2HeroesTests.swift
+//  Dota2HeroesTests
+//
+//  Created by DenisTirta on 12/08/21.
+//
+
+import XCTest
+@testable import Dota2Heroes
+
+class Dota2HeroesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
